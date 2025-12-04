ab4d92b657bb87d53dbcd2003276f75514a92cf4
Ana Garcia <ana_31412401415@utd.edu.mx>
2025-12-03 20:49:31 -0600

    03_12

--- view/InterfazPrincipalDashboard.py
+++ view/InterfazPrincipalDashboard.py
@@ -426,6 +426,11 @@ class MainInterface(ctk.CTkFrame):
 
             )
             self.historial_de_ventas.pack(fill="both" ,expand = True)
+
+
+
+
+            
     
     def crear_sidebar(self):
         self.sidebar = Sidebar(self, on_nav=self.navegar)
