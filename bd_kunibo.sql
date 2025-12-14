506a76b62cc6c8cc004de112b14abb322127e695
AnaGabrielaGarciaHernandez <ana_3141240145@utd.edu.mx>
2025-12-13 23:24:41 -0600

    Add files via upload

--- bd_kunibo.sql
+++ bd_kunibo.sql
@@ -1,200 +1,132 @@
--- MySQL dump 10.13  Distrib 8.0.43, for Win64 (x86_64)
---
--- Host: localhost    Database: bd_kunibo
--- ------------------------------------------------------
--- Server version	8.0.43
+CREATE DATABASE IF NOT EXISTS bd_kunibo;
+USE bd_kunibo;
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8mb4 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+SET FOREIGN_KEY_CHECKS = 0;
 
---
--- Table structure for table `egresos`
---
+-- =====================
+-- TABLA INSUMOS
+-- =====================
+DROP TABLE IF EXISTS insumos;
+CREATE TABLE insumos (
+  id_insumo INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  nombre_insumo TEXT NOT NULL,
+  unidad_medida TEXT NOT NULL,
+  cantidad DECIMAL(5,2) NOT NULL,
+  costo_unitario DECIMAL(5,2) NOT NULL,
+  PRIMARY KEY (id_insumo)
+);
 
-DROP TABLE IF EXISTS `egresos`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `egresos` (
-  `id_egreso` int unsigned NOT NULL AUTO_INCREMENT,
-  `id_insumo` int unsigned NOT NULL,
-  `proveedor` text,
-  `descripcion` text,
-  `monto` decimal(5,2) NOT NULL,
-  `cantidad_comprada` decimal(5,2) NOT NULL,
-  `fecha` date NOT NULL,
-  PRIMARY KEY (`id_egreso`),
-  KEY `egresos_ibfk_1` (`id_insumo`),
-  CONSTRAINT `egresos_ibfk_1` FOREIGN KEY (`id_insumo`) REFERENCES `insumos` (`id_insumo`) ON DELETE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+INSERT INTO insumos VALUES
+(1,'Vainilla','Milliliter (ml)',30.00,50.00),
+(2,'Mango','Kilogram (kg)',2.00,30.00),
+(3,'Bottle','Box',4.00,30.00),
+(4,'Fresa','Kilogram (kg)',3.00,100.00),
+(5,'Piña','Kilogram (kg)',2.00,55.00);
 
---
--- Dumping data for table `egresos`
---
+-- =====================
+-- TABLA PRODUCTOS
+-- =====================
+DROP TABLE IF EXISTS productos;
+CREATE TABLE productos (
+  id_producto INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  nombre TEXT NOT NULL,
+  tamano ENUM('pequeño','grande','Small','Large'),
+  precio DECIMAL(5,2) NOT NULL,
+  PRIMARY KEY (id_producto)
+);
 
-LOCK TABLES `egresos` WRITE;
-/*!40000 ALTER TABLE `egresos` DISABLE KEYS */;
-INSERT INTO `egresos` VALUES (1,2,'Alsuper','Mini',60.00,2.00,'2025-12-10'),(2,3,'Amazon','Actualización automática del insumo Bottle',120.00,4.00,'2025-12-10'),(3,4,'Alsuper','Fresas',300.00,3.00,'2025-12-10'),(4,5,'Alsuper','Piña',110.00,2.00,'2025-12-10');
-/*!40000 ALTER TABLE `egresos` ENABLE KEYS */;
-UNLOCK TABLES;
+INSERT INTO productos VALUES
+(1,'Fresa','Small',22.00),
+(2,'Chocolate','Small',22.00),
+(3,'Nuez','Small',22.00),
+(4,'Piña coco','Small',22.00),
+(5,'Pistache','Small',22.00),
+(6,'Capuchino','Small',22.00),
+(7,'Vainilla','Small',22.00),
+(8,'Café','Small',22.00);
 
---
--- Table structure for table `insumos`
---
+-- =====================
+-- TABLA USUARIOS
+-- =====================
+DROP TABLE IF EXISTS usuarios;
+CREATE TABLE usuarios (
+  id_usuario INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  nombre VARCHAR(50) NOT NULL,
+  apellido VARCHAR(50) NOT NULL,
+  email VARCHAR(50) NOT NULL,
+  contrasena VARCHAR(10) NOT NULL,
+  PRIMARY KEY (id_usuario)
+);
 
-DROP TABLE IF EXISTS `insumos`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `insumos` (
-  `id_insumo` int unsigned NOT NULL AUTO_INCREMENT,
-  `nombre_insumo` text NOT NULL,
-  `unidad_medida` text NOT NULL,
-  `cantidad` decimal(5,2) NOT NULL,
-  `costo_unitario` decimal(5,2) NOT NULL,
-  PRIMARY KEY (`id_insumo`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+INSERT INTO usuarios VALUES
+(1,'Gustavo','Sanchez','gus@gmail.com','123');
 
---
--- Dumping data for table `insumos`
---
+-- =====================
+-- TABLA EGRESOS
+-- =====================
+DROP TABLE IF EXISTS egresos;
+CREATE TABLE egresos (
+  id_egreso INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  id_insumo INT UNSIGNED NOT NULL,
+  proveedor TEXT,
+  descripcion TEXT,
+  monto DECIMAL(5,2) NOT NULL,
+  cantidad_comprada DECIMAL(5,2) NOT NULL,
+  fecha DATE NOT NULL,
+  PRIMARY KEY (id_egreso),
+  FOREIGN KEY (id_insumo) REFERENCES insumos(id_insumo) ON DELETE CASCADE
+);
 
-LOCK TABLES `insumos` WRITE;
-/*!40000 ALTER TABLE `insumos` DISABLE KEYS */;
-INSERT INTO `insumos` VALUES (1,'Vainilla','Milliliter (ml)',30.00,50.00),(2,'Mango','Kilogram (kg)',2.00,30.00),(3,'Bottle','Box',4.00,30.00),(4,'Fresa','Kilogram (kg)',3.00,100.00),(5,'Piña','Kilogram (kg)',2.00,55.00);
-/*!40000 ALTER TABLE `insumos` ENABLE KEYS */;
-UNLOCK TABLES;
+INSERT INTO egresos VALUES
+(1,2,'Alsuper','Mini',60.00,2.00,'2025-12-10'),
+(2,3,'Amazon','Actualización automática del insumo Bottle',120.00,4.00,'2025-12-10'),
+(3,4,'Alsuper','Fresas',300.00,3.00,'2025-12-10'),
+(4,5,'Alsuper','Piña',110.00,2.00,'2025-12-10');
 
---
--- Table structure for table `producto_insumo`
---
+-- =====================
+-- TABLA PRODUCTO_INSUMO
+-- =====================
+DROP TABLE IF EXISTS producto_insumo;
+CREATE TABLE producto_insumo (
+  id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  id_producto INT UNSIGNED NOT NULL,
+  id_insumo INT UNSIGNED NOT NULL,
+  cantidad DECIMAL(5,2) NOT NULL,
+  PRIMARY KEY (id),
+  FOREIGN KEY (id_producto) REFERENCES productos(id_producto),
+  FOREIGN KEY (id_insumo) REFERENCES insumos(id_insumo)
+);
 
-DROP TABLE IF EXISTS `producto_insumo`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `producto_insumo` (
-  `id` int unsigned NOT NULL AUTO_INCREMENT,
-  `id_producto` int unsigned NOT NULL,
-  `id_insumo` int unsigned NOT NULL,
-  `cantidad` decimal(5,2) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `id_producto` (`id_producto`),
-  KEY `id_insumo` (`id_insumo`),
-  CONSTRAINT `producto_insumo_ibfk_1` FOREIGN KEY (`id_producto`) REFERENCES `productos` (`id_producto`),
-  CONSTRAINT `producto_insumo_ibfk_2` FOREIGN KEY (`id_insumo`) REFERENCES `insumos` (`id_insumo`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+-- =====================
+-- TABLA VENTAS
+-- =====================
+DROP TABLE IF EXISTS ventas;
+CREATE TABLE ventas (
+  id_venta INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  id_usuario INT UNSIGNED NOT NULL,
+  id_producto INT UNSIGNED NOT NULL,
+  fecha_venta DATE NOT NULL,
+  cantidad INT UNSIGNED NOT NULL,
+  precio_unitario DECIMAL(5,2) NOT NULL,
+  total DECIMAL(5,2),
+  PRIMARY KEY (id_venta),
+  FOREIGN KEY (id_usuario) REFERENCES usuarios(id_usuario),
+  FOREIGN KEY (id_producto) REFERENCES productos(id_producto) ON DELETE CASCADE
+);
 
---
--- Dumping data for table `producto_insumo`
---
+INSERT INTO ventas
+(id_venta, id_usuario, id_producto, fecha_venta, cantidad, precio_unitario, total)
+VALUES
+(1,1,1,'2025-12-10',1,30.00,30.00),
+(2,1,2,'2025-12-10',3,35.00,105.00),
+(3,1,3,'2025-12-10',1,40.00,40.00),
+(4,1,4,'2025-12-10',1,42.00,42.00),
+(5,1,6,'2025-12-10',1,48.00,48.00),
+(6,1,5,'2025-12-10',1,45.00,45.00),
+(7,1,8,'2025-12-10',1,38.00,38.00),
+(8,1,7,'2025-12-10',1,32.00,32.00),
+(9,1,1,'2025-12-10',4,30.00,120.00),
+(10,1,6,'2025-12-10',2,48.00,96.00),
+(11,1,8,'2025-12-10',5,38.00,190.00);
 
-LOCK TABLES `producto_insumo` WRITE;
-/*!40000 ALTER TABLE `producto_insumo` DISABLE KEYS */;
-/*!40000 ALTER TABLE `producto_insumo` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `productos`
---
-
-DROP TABLE IF EXISTS `productos`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `productos` (
-  `id_producto` int unsigned NOT NULL AUTO_INCREMENT,
-  `nombre` text NOT NULL,
-  `tamano` enum('pequeño','grande','Small','Large') DEFAULT NULL,
-  `precio` decimal(5,2) NOT NULL,
-  PRIMARY KEY (`id_producto`)
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `productos`
---
-
-LOCK TABLES `productos` WRITE;
-/*!40000 ALTER TABLE `productos` DISABLE KEYS */;
-INSERT INTO `productos` VALUES (1,'Fresa','Small',22.00),(2,'Chocolate','Small',22.00),(3,'Nuez','Small',22.00),(4,'Piña coco','Small',22.00),(5,'Pistache','Small',22.00),(6,'Capuchino','Small',22.00),(7,'Vainilla','Small',22.00),(8,'Café','Small',22.00);
-/*!40000 ALTER TABLE `productos` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `usuarios`
---
-
-DROP TABLE IF EXISTS `usuarios`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `usuarios` (
-  `id_usuario` int unsigned NOT NULL AUTO_INCREMENT,
-  `nombre` varchar(50) NOT NULL,
-  `apellido` varchar(50) NOT NULL,
-  `email` varchar(50) NOT NULL,
-  `contrasena` varchar(10) NOT NULL,
-  PRIMARY KEY (`id_usuario`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `usuarios`
---
-
-LOCK TABLES `usuarios` WRITE;
-/*!40000 ALTER TABLE `usuarios` DISABLE KEYS */;
-INSERT INTO `usuarios` VALUES (1,'Gustavo','Sanchez','gus@gmail.com','123');
-/*!40000 ALTER TABLE `usuarios` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `ventas`
---
-
-DROP TABLE IF EXISTS `ventas`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `ventas` (
-  `id_venta` int unsigned NOT NULL AUTO_INCREMENT,
-  `id_usuario` int unsigned NOT NULL,
-  `id_producto` int unsigned NOT NULL,
-  `fecha_venta` date NOT NULL,
-  `cantidad` int unsigned NOT NULL,
-  `precio_unitario` decimal(5,2) NOT NULL,
-  `total` decimal(5,2) GENERATED ALWAYS AS ((`cantidad` * `precio_unitario`)) STORED,
-  PRIMARY KEY (`id_venta`),
-  KEY `id_usuario` (`id_usuario`),
-  KEY `ventas_ibfk_2` (`id_producto`),
-  CONSTRAINT `ventas_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `usuarios` (`id_usuario`),
-  CONSTRAINT `ventas_ibfk_2` FOREIGN KEY (`id_producto`) REFERENCES `productos` (`id_producto`) ON DELETE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `ventas`
---
-
-LOCK TABLES `ventas` WRITE;
-/*!40000 ALTER TABLE `ventas` DISABLE KEYS */;
-INSERT INTO `ventas` (`id_venta`, `id_usuario`, `id_producto`, `fecha_venta`, `cantidad`, `precio_unitario`) VALUES (1,1,1,'2025-12-10',1,30.00),(2,1,2,'2025-12-10',3,35.00),(3,1,3,'2025-12-10',1,40.00),(4,1,4,'2025-12-10',1,42.00),(5,1,6,'2025-12-10',1,48.00),(6,1,5,'2025-12-10',1,45.00),(7,1,8,'2025-12-10',1,38.00),(8,1,7,'2025-12-10',1,32.00),(9,1,1,'2025-12-10',4,30.00),(10,1,6,'2025-12-10',2,48.00),(11,1,8,'2025-12-10',5,38.00);
-/*!40000 ALTER TABLE `ventas` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2025-12-10 21:40:59
+SET FOREIGN_KEY_CHECKS = 1;
